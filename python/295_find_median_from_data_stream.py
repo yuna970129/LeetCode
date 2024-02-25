7cef29217d113c07431d5ebb60864ae508d62f3e
Yuna Kim <81917977+yuna970129@users.noreply.github.com>
2024-02-25 17:55:20 +0900

    Create 295_find_median_from_data_stream.py

---
+++ python/295_find_median_from_data_stream.py
@@ -0,0 +1,21 @@
+import heapq
+
+class MedianFinder:
+    def __init__(self):
+        self.min_heap = []
+        self.max_heap = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.max_heap, -num)
+        heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+        
+        if len(self.min_heap) > len(self.max_heap):
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+
+    def findMedian(self) -> float:
+        if len(self.max_heap) == len(self.min_heap):
+            return (self.min_heap[0] - self.max_heap[0]) / 2.0
+        else:
+            return -self.max_heap[0]
+ 
+        
